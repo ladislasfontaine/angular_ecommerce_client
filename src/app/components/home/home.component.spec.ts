f16047c3cc964d55fc56f7d50712985565913da7
ladislasfontaine <fontaine.ladislas@gmail.com>
2020-11-12 10:49:51 +0100

    adding tests

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -6,6 +6,9 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ToastrModule } from 'ngx-toastr';
 import { ProductService } from 'src/app/services/product.service';
 import { of, Observable } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -13,11 +16,18 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ],
+      declarations: [
+        HomeComponent,
+        DummyComponent
+      ],
       imports: [
         HttpClientTestingModule,
-        RouterTestingModule,
-        ToastrModule.forRoot()
+        ToastrModule.forRoot(),
+        RouterTestingModule.withRoutes(
+          [
+            { path: 'cart', component: DummyComponent }
+          ]
+        )
       ],
       providers: [
         { provide: ProductService, useClass: ProductServiceStub }
@@ -35,7 +45,33 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain an h2 tag', () => {
+    const h2Element = fixture.debugElement.query(By.css('h2'));
+    const h2HtmlElement: HTMLHeadElement = h2Element.nativeElement;
+    expect(h2HtmlElement.textContent).toBe('NEW COLLECTION');
+  });
+
+  it('should contain at least one button', () => {
+    const buttons = fixture.debugElement
+      .queryAll(By.css('button'));
+    expect(buttons.length >= 1).toBeTruthy();
+  });
+
+  it('should be a "Shop now" button first on the page', () => {
+    const buttonDes = fixture.debugElement.queryAll(By.css('button'));
+    const button: HTMLButtonElement = buttonDes[0].nativeElement;
+    expect(button.textContent).toBe('Shop now');
+  });
+
+  it('should navigate to "/" before any button click', () => {
+    const location = TestBed.inject(Location);
+    expect(location.path()).toBe('');
+  });
 });
+
+@Component({ template: '' })
+class DummyComponent {}
 
 class ProductServiceStub {
   getAllProducts(numberOfResults = 10): Observable<any[]> {
--- src/app/components/product-card/product-card.component.spec.ts
+++ src/app/components/product-card/product-card.component.spec.ts
@@ -1,18 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductCardComponent } from './product-card.component';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ToastrModule } from 'ngx-toastr';
+import { Component } from '@angular/core';
 
 describe('ProductCardComponent', () => {
   let component: ProductCardComponent;
   let fixture: ComponentFixture<ProductCardComponent>;
 
+  let testHostComponent: TestHostComponent;
+  let testHostFixture: ComponentFixture<TestHostComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [ ProductCardComponent ]
+      declarations: [ ProductCardComponent, TestHostComponent ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        ToastrModule.forRoot()
+      ]
     })
     .compileComponents();
+  });
+
+  beforeEach(() => {
+    testHostFixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = testHostFixture.componentInstance;
+    testHostFixture.detectChanges();
   });
 
   beforeEach(() => {
@@ -22,6 +37,16 @@ describe('ProductCardComponent', () => {
   });
 
   // it('should create', () => {
-  //   expect(component).toBeTruthy();
+  //   expect(testHostComponent).toBeTruthy();
   // });
+
+  @Component({
+    selector: `app-host-component`,
+    template: `<app-product-card [indexOfProduct]="1" [product]="product"></app-product-card>`
+  })
+  class TestHostComponent {
+    product: any = {
+      image: '/assets/img/products/p1.jpg'
+    };
+  }
 });
